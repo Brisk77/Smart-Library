b155da87a782e7c42b7186f382b9f6947ba8e608
LAWRENCE P. <lawrenceoghenemaro@gmail.com>
2026-03-25 10:49:53 +0100

    Update Borrowable.java

--- out/production/Library App/model/Borrowable.java
+++ out/production/Library App/model/Borrowable.java
@@ -1,4 +1,14 @@
-package com.example.library.model;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package model;
 
 public interface Borrowable {
+    void borrow();
+
+    void returnItem();
+
+    boolean isAvailable();
 }
