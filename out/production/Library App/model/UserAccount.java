fd08d87fa942c6102da70d7b3bdfd9a530f2c496
LAWRENCE P. <lawrenceoghenemaro@gmail.com>
2026-03-25 10:52:15 +0100

    Update UserAccount.java

--- out/production/Library App/model/UserAccount.java
+++ out/production/Library App/model/UserAccount.java
@@ -1,4 +1,31 @@
-package com.example.library.model;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserAccount {
+    private final String name;
+    private final List<LibraryItem> borrowedItems;
+
+    public UserAccount(String userId, String name) {
+        this.name = name;
+        this.borrowedItems = new ArrayList();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void borrowItem(LibraryItem item) {
+        this.borrowedItems.add(item);
+    }
+
+    public void returnItem(LibraryItem item) {
+        this.borrowedItems.remove(item);
+    }
 }
