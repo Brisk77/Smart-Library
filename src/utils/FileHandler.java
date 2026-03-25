dfb4c799e1b568a93701b40e1bb72910fd5ee5a7
LAWRENCE P. <lawrenceoghenemaro@gmail.com>
2026-03-25 10:16:32 +0100

    Update FileHandler.java

--- src/utils/FileHandler.java
+++ src/utils/FileHandler.java
@@ -1,4 +1,16 @@
 package utils;
 
+import model.LibraryDatabase;
+
 public class FileHandler {
+    public static void saveData(LibraryDatabase database, String filePath) {
+        // Implementation to save data to a file (e.g., JSON or serialized objects)
+        System.out.println("Saving data to " + filePath);
+    }
+
+    public static LibraryDatabase loadData(String filePath) {
+        // Implementation to load data from a file
+        System.out.println("Loading data from " + filePath);
+        return new LibraryDatabase();
+    }
 }
