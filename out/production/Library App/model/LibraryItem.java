42f7b0f42e79969020a522cc705db2d694115c1a
LAWRENCE P. <lawrenceoghenemaro@gmail.com>
2026-03-25 10:51:11 +0100

    Update LibraryItem.java

--- out/production/Library App/model/LibraryItem.java
+++ out/production/Library App/model/LibraryItem.java
@@ -1,4 +1,50 @@
-package com.example.library.model;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
-public class LibraryItem {
+package model;
+
+public abstract class LibraryItem {
+    private String id;
+    private String title;
+    private String author;
+    private int year;
+    private boolean isAvailable;
+
+    public LibraryItem(String id, String title, String author, int year) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.isAvailable = true;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public boolean isAvailable() {
+        return this.isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        this.isAvailable = available;
+    }
+
+    public abstract String getType();
+
+    public abstract int getBorrowDuration();
 }
