0da9e657bc34864e99839389d00ae557401e1d82
LAWRENCE P. <lawrenceoghenemaro@gmail.com>
2026-03-25 10:50:40 +0100

    Update LibraryDatabase.java

--- out/production/Library App/model/LibraryDatabase.java
+++ out/production/Library App/model/LibraryDatabase.java
@@ -1,4 +1,35 @@
-package com.example.library.model;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LibraryDatabase {
+    private List<LibraryItem> items = new ArrayList();
+
+    public void addItem(LibraryItem item) {
+        this.items.add(item);
+    }
+
+    public void removeItem(LibraryItem item) {
+        this.items.remove(item);
+    }
+
+    public LibraryItem findItemById(String id) {
+        for(LibraryItem item : this.items) {
+            if (item.getId().equals(id)) {
+                return item;
+            }
+        }
+
+        return null;
+    }
+
+    public List<LibraryItem> getAllItems() {
+        return new ArrayList(this.items);
+    }
 }
