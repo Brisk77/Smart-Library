e0de71fbfd986fc065de3f98f5dfeba111ed163b
LAWRENCE P. <lawrenceoghenemaro@gmail.com>
2026-03-25 10:18:05 +0100

    Refactor Journal class and add Book class

--- src/model/Journal.java
+++ src/model/Journal.java
@@ -1,4 +1,25 @@
-package com.example.library.model;
+package model;
 
-public class Journal {
+public class Book extends LibraryItem {
+    private String isbn;
+    private static final int BORROW_DURATION = 21; // 3 weeks
+
+    public Book(String id, String title, String author, int year, String isbn) {
+        super(id, title, author, year);
+        this.isbn = isbn;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    @Override
+    public String getType() {
+        return "Book";
+    }
+
+    @Override
+    public int getBorrowDuration() {
+        return BORROW_DURATION;
+    }
 }
