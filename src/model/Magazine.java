3b77fda3a24fadad16ee48f41d992213f924e0dc
LAWRENCE P. <lawrenceoghenemaro@gmail.com>
2026-03-25 10:19:59 +0100

    Implement Magazine class extending LibraryItem

--- src/model/Magazine.java
+++ src/model/Magazine.java
@@ -1,4 +1,25 @@
-package com.example.library.model;
+package model;
 
-public class Magazine {
+public class Magazine extends LibraryItem {
+    private int issueNumber;
+    private static final int BORROW_DURATION = 7; // 1 week
+
+    public Magazine(String id, String title, String author, int year, int issueNumber) {
+        super(id, title, author, year);
+        this.issueNumber = issueNumber;
+    }
+
+    public int getIssueNumber() {
+        return issueNumber;
+    }
+
+    @Override
+    public String getType() {
+        return "Magazine";
+    }
+
+    @Override
+    public int getBorrowDuration() {
+        return BORROW_DURATION;
+    }
 }
