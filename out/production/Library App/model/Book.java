30696bc5f8847534fb883fb3f76425905a3a086b
LAWRENCE P. <lawrenceoghenemaro@gmail.com>
2026-03-25 10:49:29 +0100

    Update Book.java

--- out/production/Library App/model/Book.java
+++ out/production/Library App/model/Book.java
@@ -1,4 +1,28 @@
-package com.example.library.model;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
-public class Book {
+package model;
+
+public class Book extends LibraryItem {
+    private String isbn;
+    private static final int BORROW_DURATION = 21;
+
+    public Book(String id, String title, String author, int year, String isbn) {
+        super(id, title, author, year);
+        this.isbn = isbn;
+    }
+
+    public String getIsbn() {
+        return this.isbn;
+    }
+
+    public String getType() {
+        return "Book";
+    }
+
+    public int getBorrowDuration() {
+        return 21;
+    }
 }
