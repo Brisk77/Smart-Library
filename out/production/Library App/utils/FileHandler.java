1df9634271e06b913a2ff00b49aab2ca33903af0
LAWRENCE P. <lawrenceoghenemaro@gmail.com>
2026-03-25 10:52:44 +0100

    Update FileHandler.java

--- out/production/Library App/utils/FileHandler.java
+++ out/production/Library App/utils/FileHandler.java
@@ -1,4 +1,19 @@
-package com.example.library.utils;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package utils;
+
+import model.LibraryDatabase;
 
 public class FileHandler {
+    public static void saveData(LibraryDatabase database, String filePath) {
+        System.out.println("Saving data to " + filePath);
+    }
+
+    public static LibraryDatabase loadData(String filePath) {
+        System.out.println("Loading data from " + filePath);
+        return new LibraryDatabase();
+    }
 }
