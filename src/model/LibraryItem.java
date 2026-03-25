899fbd9be3ad89c107b23cae64eff8a9d2603ac8
LAWRENCE P. <lawrenceoghenemaro@gmail.com>
2026-03-25 10:19:07 +0100

    Update LibraryItem.java

--- src/model/LibraryItem.java
+++ src/model/LibraryItem.java
@@ -1,4 +1,47 @@
-package com.example.library.model;
+package model;
 
-public class LibraryItem {
+public abstract class LibraryItem {
+    private String id;
+    private String title;
+    private String author;
+    private int year;
+    private boolean isAvailable;
+
+    public LibraryItem(String id, String title, String author, int year) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.isAvailable = true;
+    }
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    // Setters
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    // Abstract methods to be implemented by subclasses
+    public abstract String getType();
+    public abstract int getBorrowDuration();
 }
