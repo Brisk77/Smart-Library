48fa196b895e844f68b8e75c25c22d047f9709db
LAWRENCE P. <lawrenceoghenemaro@gmail.com>
2026-03-25 10:51:33 +0100

    Update Magazine.java

--- out/production/Library App/model/Magazine.java
+++ out/production/Library App/model/Magazine.java
@@ -1,4 +1,28 @@
-package com.example.library.model;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
-public class Magazine {
+package model;
+
+public class Magazine extends LibraryItem {
+    private int issueNumber;
+    private static final int BORROW_DURATION = 7;
+
+    public Magazine(String id, String title, String author, int year, int issueNumber) {
+        super(id, title, author, year);
+        this.issueNumber = issueNumber;
+    }
+
+    public int getIssueNumber() {
+        return this.issueNumber;
+    }
+
+    public String getType() {
+        return "Magazine";
+    }
+
+    public int getBorrowDuration() {
+        return 7;
+    }
 }
