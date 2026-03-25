f5a43382cc9ada83e2eceb58762ac1db8b6eab5b
LAWRENCE P. <lawrenceoghenemaro@gmail.com>
2026-03-25 10:50:16 +0100

    Update Journal.java

--- out/production/Library App/model/Journal.java
+++ out/production/Library App/model/Journal.java
@@ -1,4 +1,28 @@
-package com.example.library.model;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
-public class Journal {
+package model;
+
+public class Journal extends LibraryItem {
+    private String conferenceName;
+    private static final int BORROW_DURATION = 14;
+
+    public Journal(String id, String title, String author, int year, String conferenceName) {
+        super(id, title, author, year);
+        this.conferenceName = conferenceName;
+    }
+
+    public String getConferenceName() {
+        return this.conferenceName;
+    }
+
+    public String getType() {
+        return "Journal";
+    }
+
+    public int getBorrowDuration() {
+        return 14;
+    }
 }
