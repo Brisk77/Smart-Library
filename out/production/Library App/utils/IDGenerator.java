03eb044f7eca9d2f49993b67c8856e2a5176dd7c
LAWRENCE P. <lawrenceoghenemaro@gmail.com>
2026-03-28 11:49:09 +0100

    Update IDGenerator.java

--- out/production/Library App/utils/IDGenerator.java
+++ out/production/Library App/utils/IDGenerator.java
@@ -1,4 +1,14 @@
-package com.example.library.utils;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package utils;
+
+import java.util.UUID;
 
 public class IDGenerator {
+    public static String generateUniqueId() {
+        return UUID.randomUUID().toString();
+    }
 }
