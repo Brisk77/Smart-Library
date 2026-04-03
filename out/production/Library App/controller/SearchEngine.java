f28c431fb340dfcd4dc93c594bd7c0f824a8f5d5
LAWRENCE P. <lawrenceoghenemaro@gmail.com>
2026-04-03 06:25:15 +0100

    Update SearchEngine.java

--- out/production/Library App/controller/SearchEngine.java
+++ out/production/Library App/controller/SearchEngine.java
@@ -1,4 +1,24 @@
-package com.example.library.controller;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.LibraryItem;
 
 public class SearchEngine {
+    public static List<LibraryItem> linearSearchByTitle(List<LibraryItem> items, String title) {
+        List<LibraryItem> result = new ArrayList();
+
+        for(LibraryItem item : items) {
+            if (item.getTitle().equalsIgnoreCase(title)) {
+                result.add(item);
+            }
+        }
+
+        return result;
+    }
 }
