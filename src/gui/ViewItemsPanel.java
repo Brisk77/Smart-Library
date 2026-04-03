6b93ef2de7ea06ba27cd3403c99ad47057a45805
LAWRENCE P. <lawrenceoghenemaro@gmail.com>
2026-04-03 06:21:13 +0100

    Update ViewItemsPanel.java

--- src/gui/ViewItemsPanel.java
+++ src/gui/ViewItemsPanel.java
@@ -1,4 +1,19 @@
-package com.example.library.gui;
+package gui;
 
-public class ViewItemsPanel {
+import controller.LibraryManager;
+import javax.swing.*;
+import java.awt.*;
+
+public class ViewItemsPanel extends JPanel {
+
+    public ViewItemsPanel(LibraryManager libraryManager) {
+        setLayout(new BorderLayout());
+
+        // Create a table to display library items
+        LibraryItemTableModel model = new LibraryItemTableModel(libraryManager.getDatabase().getAllItems());
+        JTable table = new JTable(model);
+        JScrollPane scrollPane = new JScrollPane(table);
+
+        add(scrollPane, BorderLayout.CENTER);
+    }
 }
